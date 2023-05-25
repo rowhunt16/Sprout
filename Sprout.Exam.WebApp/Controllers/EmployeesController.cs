878824553afa921e0975e2096bea41497ea18b0a
Roanne May <Roanne May@DESKTOP-40UUGG8>
2023-05-25 11:38:42 +0800

    Add project files.

---
+++ Sprout.Exam.WebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Sprout.Exam.Business.DataTransferObjects;
+using Sprout.Exam.Common.Enums;
+using Sprout.Exam.WebApp.Models;
+
+namespace Sprout.Exam.WebApp.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and fetch from the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await Task.FromResult(StaticEmployees.ResultList);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and fetch from the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == id));
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and update changes to the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(EditEmployeeDto input)
+        {
+            var item = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == input.Id));
+            if (item == null) return NotFound();
+            item.FullName = input.FullName;
+            item.Tin = input.Tin;
+            item.Birthdate = input.Birthdate.ToString("yyyy-MM-dd");
+            item.TypeId = input.TypeId;
+            return Ok(item);
+        }
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and insert employees to the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateEmployeeDto input)
+        {
+
+           var id = await Task.FromResult(StaticEmployees.ResultList.Max(m => m.Id) + 1);
+
+            StaticEmployees.ResultList.Add(new EmployeeDto
+            {
+                Birthdate = input.Birthdate.ToString("yyyy-MM-dd"),
+                FullName = input.FullName,
+                Id = id,
+                Tin = input.Tin,
+                TypeId = input.TypeId
+            });
+
+            return Created($"/api/employees/{id}", id);
+        }
+
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and perform soft deletion of an employee to the DB.
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == id));
+            if (result == null) return NotFound();
+            StaticEmployees.ResultList.RemoveAll(m => m.Id == id);
+            return Ok(id);
+        }
+
+
+
+        /// <summary>
+        /// Refactor this method to go through proper layers and use Factory pattern
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="absentDays"></param>
+        /// <param name="workedDays"></param>
+        /// <returns></returns>
+        [HttpPost("calculate")]
+        public async Task<IActionResult> Calculate([FromBody] SalaryCompModel model)
+        {
+            var result = await Task.FromResult(StaticEmployees.ResultList.FirstOrDefault(m => m.Id == model.Id));
+
+            if (result == null) return NotFound();
+            var type = (EmployeeType) result.TypeId;
+            decimal finalSalary = 0.00M;
+            decimal grossSalary = 20000.00m;
+            decimal dailyRate = 500.00m;
+            decimal tax = 0.12M;
+
+            switch (type)
+            {
+                case EmployeeType.Regular:
+                    finalSalary = model.AbsentDays > 0 ? grossSalary - (grossSalary / 22 * model.AbsentDays) - (grossSalary * tax) : grossSalary - (grossSalary * tax);
+                    //finalSalary = (grossSalary / 22 * model.AbsentDays);
+                    break;
+                case EmployeeType.Contractual:
+                    finalSalary = dailyRate * model.WorkedDays;
+                    break;
+                default:
+                    return NotFound("Employee Type not found");
+            };
+
+            return Ok(finalSalary);
+
+        }
+
+    }
+}
---
+++ Sprout.Exam.WebApp/Models/SalaryCompModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sprout.Exam.WebApp.Models
+{
+    public class SalaryCompModel
+    {
+        public int Id { get; set; }
+        public decimal AbsentDays { get; set; }
+        public decimal WorkedDays { get; set; }
+    }
+}
